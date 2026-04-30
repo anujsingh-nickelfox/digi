7ee9b03674653023f58d33b9fca60f4241403c0c
Anuj Singh <your-email@example.com>
2026-04-30 13:39:31 +0530

    chore: prepare server for deployment

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(
     origin: [
       "http://localhost:5173", // Vite default
       "http://localhost:3000", // Alternative local port
+      "https://digi-edu.vercel.app"
     ],
     methods: ["GET", "POST"],
     credentials: true,
